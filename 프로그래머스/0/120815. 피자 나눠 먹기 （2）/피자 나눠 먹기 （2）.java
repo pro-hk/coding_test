57b53aead1f7b376ac9bcfcd26945b5a59e8782e
pro-hk <93908050+pro-hk@users.noreply.github.com>
2026-06-02 11:50:30 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 65.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        while(true) {
+            answer++;
+            if((answer * 6) % n == 0) break;
+        }
+        
+        return answer;
+    }
+}
